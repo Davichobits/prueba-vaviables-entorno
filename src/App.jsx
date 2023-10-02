495367b8c612d841e5b552de6439473c8904d627
Davichobits <davidruizdeveloper@gmail.com>
2023-10-02 16:13:13 -0500

    peticion POST con variable de entorno

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,21 @@ console.log();
 function App() {
   const [msg, setMsg] = useState('')
 
+  const object = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "email": "demo@minimals.cc",
+      "password": import.meta.env.VITE_SECRET_KEY
+    })
+  }
+
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${import.meta.env.VITE_SECRET_KEY}/`)
+    fetch(`https://api-dev-minimal-v510.vercel.app/api/auth/login`, object)
       .then(response => response.json())
-      .then(data => setMsg(data.name))
+      .then(data => setMsg(data.user.displayName))
   }, []);
 
   return (
